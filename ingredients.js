957841a1c649f2e9820e386bc057817f37411e77
Jed Salonga <jed.salonga@hotmail.com>
2022-10-27 18:41:09 -0700

    iterating over arrays challenge

---
+++ ingredients.js
@@ -0,0 +1,23 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+console.log("Ingredients");
+	let i = 0
+while (i < ingredients.Length) {
+	console.log(ingredients[i]);
+	i++;
+}
+
+
+// Write a for loop that prints out the contents of ingredients:
+console.log("Ingredients");
+	for (let i = 0; i < ingredients.length; i++)
+	console.log(ingredients[i]);
+
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+console.log("Ingredients")
+	for (let i = ingredients.Length - 1; i >= 0; i--) {
+	console.log(ingredients[i]);
+}
